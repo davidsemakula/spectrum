06fd2a199186175c83fdbfd9cba1953534ab453d
SAPikachu <sapikachu@gmail.com>
2019-01-28 18:14:21 +0800

    Fix another hardcoded imgix domain

--- api/mutations/thread/editThread.js
+++ api/mutations/thread/editThread.js
@@ -21,6 +21,10 @@ type Input = {
   input: EditThreadInput,
 };
 
+const IMGIX_DOMAIN = process.env.IMGIX_DOMAIN || 'spectrum.imgix.net';
+const IMGIX_PROXY_DOMAIN =
+  process.env.IMGIX_PROXY_DOMAIN || 'spectrum-proxy.imgix.net';
+
 export default requireAuth(async (_: any, args: Input, ctx: GraphQLContext) => {
   const { input } = args;
   const { user } = ctx;
@@ -104,8 +108,8 @@ export default requireAuth(async (_: any, args: Input, ctx: GraphQLContext) => {
 
     const stripQueryParams = (str: string): string => {
       if (
-        str.indexOf('https://spectrum.imgix.net') < 0 &&
-        str.indexOf('https://spectrum-proxy.imgix.net') < 0
+        str.indexOf(`https://${IMGIX_DOMAIN}`) < 0 &&
+        str.indexOf(`https://${IMGIX_PROXY_DOMAIN}`) < 0
       ) {
         return str;
       }
